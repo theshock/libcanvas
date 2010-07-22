d21ac58395aabccbe1888fa307b28ef4213616d5
Shock <shocksilien@gmail.com>
2010-07-22 04:29:38 +0300

    8 cars

--- js/Strategy/Start.js
+++ js/Strategy/Start.js
@@ -34,14 +34,16 @@ window.addEvent('domready', function () {
 			});
 
 			strategy.createMap([
-				[1, 1, 1, 1, 1, 1, 1, 2],
-				[1, 1, 2, 2, 2, 2, 2, 2],
-				[2, 2, 2, 2, 3, 3, 3, 3],
-				[3, 3, 3, 3, 3, 3, 3, 3],
-				[3, 3, 3, 1, 1, 1, 3, 3],
-				[3, 3, 1, 1, 3, 1, 3, 3],
-				[3, 3, 1, 3, 3, 3, 3, 3],
-				[3, 3, 3, 3, 3, 3, 3, 3]
+				[1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2],
+				[1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2],
+				[2, 2, 2, 2, 3, 3, 3, 2, 2, 2, 2, 3, 3, 3, 3, 3],
+				[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+				[3, 3, 3, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+				[3, 3, 1, 1, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+				[3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3],
+				[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 3, 3],
+				[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 3, 3, 3],
+				[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 			]);
 
 			strategy.listenMouse();
@@ -50,12 +52,28 @@ window.addEvent('domready', function () {
 
 			canvas.addElement(strategy);
 
-			var car = new LibCanvas.Engines.TopDown
-				.Unit(strategy, carImage)
-				.setCoord(new LibCanvas.Point(3, 3))
-				.setZIndex(10);
+			var cars = [];
 
-			car.type = 'land';
+			(8).times(function () {
+				var car = new LibCanvas.Engines.TopDown
+					.Unit(strategy, carImage)
+					.setZIndex(10);
+
+				car.type = 'land';
+				canvas.addElement(car);
+				cars.push(car);
+				car.bind(['move', 'rotate'], function () {
+					canvas.update();
+				});
+			});
+			cars[0].setCoord(new LibCanvas.Point( 2, 2));
+			cars[1].setCoord(new LibCanvas.Point( 3, 3));
+			cars[2].setCoord(new LibCanvas.Point( 6, 3));
+			cars[3].setCoord(new LibCanvas.Point( 6, 8));
+			cars[4].setCoord(new LibCanvas.Point(15, 2));
+			cars[5].setCoord(new LibCanvas.Point(15, 7));
+			cars[6].setCoord(new LibCanvas.Point( 1, 7));
+			cars[7].setCoord(new LibCanvas.Point( 5, 7));
 
 			var here = new LibCanvas.Engines.TopDown
 				.Unit(strategy, hereImage)
@@ -64,32 +82,31 @@ window.addEvent('domready', function () {
 
 			here.type = 'land';
 
-			canvas.addElement(car);
 			canvas.addElement(here);
 
 
-			car.bind(['move', 'rotate'], function () {
-				canvas.update();
-			});
 
 			strategy.bind('click', function (event, e) {
 				here.moveTo(strategy.map.getCell({x:e.offsetX, y:e.offsetY}));
-				var step = function () {
+				var step = function (car) {
 					var diff = car.cellCoord.diff(here.cellCoord);
 					if (!diff.x && !diff.y) {
 						return;
 					}
+					var s = function () {
+						step(car);
+					};
 					if (diff.x.abs() > diff.y.abs()) {
 						car.rotateTo(diff.x > 0 ? 'right' : 'left', function () {
-							car.move(step);
+							car.move(s);
 						});
 					} else {
 						car.rotateTo(diff.y > 0 ? 'bottom' : 'top', function () {
-							car.move(step);
+							car.move(s);
 						});
 					}
 				};
-				step();
+				cars.each(step);
 				canvas.update();
 			});
 		}
