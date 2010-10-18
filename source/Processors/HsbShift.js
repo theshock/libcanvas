2f25794069da018030ffcbbca6f7d1d694873683
Shock <shocksilien@gmail.com>
2010-10-18 07:49:25 +0300

    HsbShift processor fixed

--- source/Processors/HsbShift.js
+++ source/Processors/HsbShift.js
@@ -9,7 +9,7 @@ LibCanvas.Processors.HsbShift = new Class({
 			this.shift %= 360;
 			if (this.shift < 0) this.shift += 360;
 		} else {
-			this.shift = this.shift.limit(0, 100);
+			this.shift = this.shift.limit(-100, 100);
 		}
 	},
 	processPixels : function (data) {
@@ -19,12 +19,14 @@ LibCanvas.Processors.HsbShift = new Class({
 			key   = { hue: 0, sat: 1, bri: 2 }[param],
 			i, hsb, rgb;
 		for (i = 0; i < d.length; i+=4) {
-			if (param == 'hue' && d[i] == d[i+1] && d[i] == d[i+2]) continue;
+			if (['hue', 'sat'].contains(param) && d[i] == d[i+1] && d[i] == d[i+2]) continue;
+
 			hsb = [d[i], d[i+1], d[i+2]].rgbToHsb();
 			param == 'hue' ?
-				hsb[0] = (hsb[0] + shift) % 360 :
-				hsb[key] = (hsb[key] + shift).limit(0, 100);
+				(hsb[0  ] = (hsb[0]   + shift) % 360) :
+				(hsb[key] = (hsb[key] + shift).limit(0, 100));
 			rgb    = hsb.hsbToRgb();
+
 			d[i  ] = rgb[0];
 			d[i+1] = rgb[1];
 			d[i+2] = rgb[2];
