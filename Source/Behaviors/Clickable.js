787492c7c006cb92eed756f0dd8e81a8b37ac5d7
Shock <shocksilien@gmail.com>
2011-02-02 00:44:30 +0200

    Examples: Ui.Draggable

--- Source/Behaviors/Clickable.js
+++ Source/Behaviors/Clickable.js
@@ -33,7 +33,7 @@ LibCanvas.namespace('Behaviors').Clickable = atom.Class({
 	clickable : function () { 
 		this.listenMouse();
 
-		var fn = setValFn.bind(null, this);
+		var fn = setValFn.context(null, [this]);
 		
 		this.addEvent('mouseover', fn('hover', true));
 		this.addEvent('mouseout' , fn('hover', false));
