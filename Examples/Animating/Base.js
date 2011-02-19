b49555f5847cab71963431fab01d7f14c2944576
Shock <shocksilien@gmail.com>
2011-02-20 01:59:56 +0200

    Animatable.base example

---
+++ Examples/Animating/Base.js
@@ -0,0 +1,40 @@
+/*
+---
+
+name: "Animating.Base"
+
+description: "Animating.Base"
+
+license: "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
+
+authors:
+	- "Shock <shocksilien@gmail.com>"
+
+requires:
+	- Examples
+
+provides: Animating.Base
+
+...
+*/
+
+LibCanvas.Examples.set('Animating.Base', function (canvas) {
+	var LC = LibCanvas.extract({});
+
+	var libcanvas = new LibCanvas(canvas, { clear: true });
+
+	var grip = libcanvas.start()
+		.createGrip({
+			shape : new LC.Circle(150, 75, 15),
+			fill  : '#900',
+			stroke: '#f00'
+		});
+
+	(function () {
+		new LC.Animatable(grip.shape)
+			.animate({
+				radius: 75
+			});
+	}.delay(2000));
+
+});
