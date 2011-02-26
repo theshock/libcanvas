eee10a7a182dd495db364df732dff236140ebbaa
Shock <shocksilien@gmail.com>
2011-02-26 04:55:59 +0200

    line.intersect

--- Source/Shapes/Line.js
+++ Source/Shapes/Line.js
@@ -25,7 +25,10 @@ new function () {
 var Point = LibCanvas.Point,
 	math = Math,
 	max = math.max,
-	min = math.min;
+	min = math.min,
+	between = function (x, a, b) {
+		return x === a || x === b || (a < x && x < b) || (b < x && x < a);
+	};
 
 
 LibCanvas.namespace('Shapes').Line = atom.Class({
@@ -52,6 +55,24 @@ LibCanvas.namespace('Shapes').Line = atom.Class({
 
 		// if triangle square is zero - points are on one line
 		return ((fx-px)*(ty-py)-(tx-px)*(fy-py)).round(6) == 0;
+	},
+	intersect: function (line) {
+		line = this.self.from(arguments);
+		var a = this.from, b = this.to, c = line.from, d = line.t, x, y;
+		if (d.x == c.x) { // DC == vertical line
+			if (b.x == a.x) {
+				return a.x == d.x && (a.y.between(c.y, d.y) || b.x.between(c.y, d.y));
+			}
+			x = d.x;
+			y = b.y + (x-b.x)*(a.y-b.y)/(a.x-b.x);
+		} else {
+			x = ((a.x*b.y - b.x*a.y)*(d.x-c.x)-(c.x*d.y - d.x*c.y)*(b.x-a.x))/((a.y-b.y)*(d.x-c.x)-(c.y-d.y)*(b.x-a.x));
+			y = ((c.y-d.y)*x-(c.x*d.y-d.x*c.y))/(d.x-c.x);
+			x *= -1;
+		}
+		
+		return between(x, a.x, b.x) && between (y, a.y, b.y) &&
+		       between(x, c.x, d.x) && between (y, c.y, d.y);
 	},
 	get length () {
 		return this.to.distanceTo(this.from);
