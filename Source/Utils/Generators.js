4db82cee348373fb2a5b24b85084687e1bfd5c95
Shock <shocksilien@gmail.com>
2011-03-01 05:39:07 +0200

    Generators

--- Source/Utils/Generators.js
+++ Source/Utils/Generators.js
@@ -17,15 +17,15 @@ provides: atom.Class.Mutators.Generators
 
 new function () {
 
-var getter = function (obj, key, fn) {
+var getter = function (key, fn) {
 	return function() {
-		var pr = '_' + key;
+		var pr = '_' + key, obj = this;
 		return pr in obj ? obj[pr] : (obj[pr] = fn.call(obj));
 	};
 };
 
 atom.Class.Mutators.Generators = function(properties) {
-	for (var i in properties) this.__defineGetter(i, getter(this, i, properties[i]));
+	for (var i in properties) this.prototype.__defineGetter__(i, getter(i, properties[i]));
 };
 
 };
