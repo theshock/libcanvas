5f9dbd72abc9e88cb1efdd2297a55abe91647ff2
Shock <shocksilien@gmail.com>
2011-03-01 05:06:09 +0200

    atom.Class.Mutators.Generators garbage collect

--- Source/Utils/Generators.js
+++ Source/Utils/Generators.js
@@ -15,16 +15,17 @@ provides: atom.Class.Mutators.Generators
 ...
 */
 
+new function () {
+
+var getter = function (obj, key, fn) {
+	return function() {
+		var pr = '_' + key;
+		return pr in obj ? obj[pr] : (obj[pr] = fn.call(obj));
+	};
+};
+
 atom.Class.Mutators.Generators = function(properties) {
-	var self = this;
-	for (var i in properties) {
-		(function (i, fn) {
-			self.__defineGetter(i, function() {
-				var pr = '_' + i;
-				return pr in self ? self[pr] : (self[pr] = fn.call(self));
-				
-			});
-		})(i, properties[i]);
-	}
-	properties = null;
+	for (var i in properties) this.__defineGetter(i, getter(this, i, properties[i]));
+};
+
 };
