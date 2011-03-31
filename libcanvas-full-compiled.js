e5a8fd02ec0fd341c90132d55b2b05d811b0a6b6
Shock <shocksilien@gmail.com>
2011-03-31 04:33:19 +0300

    Recompile

--- libcanvas-full-compiled.js
+++ libcanvas-full-compiled.js
@@ -4300,7 +4300,7 @@ LibCanvas.namespace('Engines').Tile = atom.Class({
 		return this.rects[cell.x + '.' + cell.y];
 	},
 	getCell : function (point) {
-		point = Point.from(arguments);
+		point = LibCanvas.Point.from(arguments);
 		var x = parseInt(point.x / (this.cellWidth  + this.margin)),
 			y = parseInt(point.y / (this.cellHeight + this.margin)),
 			row = this.matrix[y];
