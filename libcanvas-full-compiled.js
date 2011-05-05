1f4d65ea58ad81dcee9f1186a95446db31312608
Shock <shocksilien@gmail.com>
2011-05-05 05:28:49 +0300

    StopWatch: with format

--- libcanvas-full-compiled.js
+++ libcanvas-full-compiled.js
@@ -5997,16 +5997,25 @@ LibCanvas.namespace('Utils').StopWatch = atom.Class({
 		this.startTime = 0;
 		return this;
 	},
-	getTime : function (micro) {
+	getTime : function (format) {
 		var d2 = function (num) { return num < 10 ? '0' + num : num; };
 
 		var t = Date.now() - this.startTime;
 
-		if (micro) return t;
+		if (format === true) return t;
+
 		var s = (t / 1000).round(), m = (s / 60).round(), h = (m / 60).round();
-		return (s < 60) ?
-			d2((t / 1000).toFixed(1)) :
-			h + ':' + d2(m) + ':' + d2(s % 60);
+		if (typeof format === 'string') {
+			return format.substitute({
+				S: s, M: m, H: h,
+				S2: d2(s), M2: d2(s), H2: d2(s),
+				s: d2(s % 60), m: d2(m % 60), h: h
+			});
+		} else {
+			return (s < 60) ?
+				d2((t / 1000).toFixed(1)) :
+				h + ':' + d2(m % 60) + ':' + d2(s % 60);
+		}
 	},
 	toString: Function.lambda('[object LibCanvas.Utils.StopWatch]')
 });
