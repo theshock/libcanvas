acf5a3e4440e1b40373a3cdcb358af239353f4f4
Shock <shocksilien@gmail.com>
2011-05-27 21:42:40 +0300

    opti

--- Source/Core/Curves.js
+++ Source/Core/Curves.js
@@ -143,10 +143,12 @@ LibCanvas.Context2D.implement({
 			}
 			
 			for(var d=0;d<=dist;d+=0.4){
+				
 				point.x = last.x + cos * d;
 				point.y = last.y + sin * d;
 				
-				for(w=0; w<=width; w++){
+				// for(w=0;w<=width;w++){ // без сглаживания
+				for(w=0;w<width+1;w++){
 					dx = sin * w;
 					dy = cos * w;
 					
