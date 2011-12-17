0a625db64818a983fdf3a9597b8d52931ff4a0ef
TheShock <shocksilien@gmail.com>
2011-12-17 13:40:37 +0400

    IsometricProjection

---
+++ Source/Engines/IsometricProjection.js
@@ -0,0 +1,47 @@
+LibCanvas.Engines.IsometricProjection = atom.Class(
+/** @lends LibCanvas.Engines.IsometricProjection# */
+{
+
+	/**
+	 * factor (and default factor in proto)
+	 * @property {Point3D}
+	 */
+	factor: [0.866, 0.5, 0.866],
+
+	/**
+	 * @constructs
+	 * @param {Point3D} factor
+	 */
+	initialize: function (factor) {
+		atom.Class.bindAll( this );
+		this.factor = Point3D( factor || this.factor );
+	},
+
+	/**
+	 * @param {Point3D} point3d
+	 * @returns {Point}
+	 */
+	toIsometric: function (point3d) {
+		point3d = Point3D( point3d );
+		return new Point(
+			(point3d.y + point3d.x) * this.factor.x,
+			(point3d.y - point3d.x) * this.factor.y - point3d.z * this.factor.z
+		);
+	},
+
+	/**
+	 * @param {Point} point
+	 * @param {Number} z = 0
+	 * @returns {Point3D}
+	 */
+	to3D: function (point, z) {
+		point = Point(point);
+		z = Number(z) || 0;
+
+		var
+			dXY = (point.y + z * this.factor.z) / this.factor.y,
+			pX  = (point.x / this.factor.x - dXY) / 2;
+
+		return new Point3D( pX, pX + dXY, z );
+	}
+});
