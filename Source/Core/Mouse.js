67ea6912956e4c3566e3232c53ee4dda529d5d9a
TheShock <shocksilien@gmail.com>
2012-02-04 21:23:12 +0200

    Mouse refactoring

--- Source/Core/Mouse.js
+++ Source/Core/Mouse.js
@@ -23,245 +23,135 @@ provides: Mouse
 */
 
 
-var Mouse = declare( 'LibCanvas.Mouse',
-/**
- * @lends LibCanvas.Mouse.prototype
- * @augments Class.Events.prototype
- */
-{
-	mixin: [ Events.Mixin ],
-	
-	own: {
-		buttons: {
-			left  : false,
-			middle: false,
-			// not supported
-			right : false
-		},
-		listen: function () {
-			var Mouse   = this,
-				buttons = Mouse.buttons,
-				set = function (val, b) {
-					return function (e) {
-						if (b == 'all') {
-							for (var i in buttons) buttons[i] = val
-						} else {
-							b = b || ['left', 'middle'][e.button];
-							if (b) buttons[b] = val;
-						}
-					};
-				};
-			atom.dom().bind({
-				mousedown  : set(true),
-				mouseup    : set(false),
-				blur       : set(false, 'all')
-			});
-		},
-		createOffset : function(elem) {
-			var top = 0, left = 0;
-			if (elem.getBoundingClientRect) {
-				var box = elem.getBoundingClientRect();
+var Mouse = new function () {
+	function eventSource (e) {
+		return e.changedTouches ? e.changedTouches[0] : e;
+	}
 
-				// (2)
-				var body    = document.body;
-				var docElem = document.documentElement;
+	return declare( 'LibCanvas.Mouse', {
+		own: {
+			expandEvent: function (e) {
+				var source = eventSource(e);
 
-				// (3)
-				var scrollTop  = window.pageYOffset || docElem.scrollTop  || body.scrollTop;
-				var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+				e.pageX = source.pageX != null ? source.pageX : source.clientX + document.scrollLeft;
+				e.pageY = source.pageY != null ? source.pageY : source.clientY + document.scrollTop ;
 
-				// (4)
-				var clientTop  = docElem.clientTop  || body.clientTop  || 0;
-				var clientLeft = docElem.clientLeft || body.clientLeft || 0;
-
-				// (5)
-				top  = box.top  + scrollTop  - clientTop;
-				left = box.left + scrollLeft - clientLeft;
-
-				return { top: top.round(), left: left.round() };
-			} else {
-				while(elem) {
-					top  = top  + parseInt(elem.offsetTop);
-					left = left + parseInt(elem.offsetLeft);
-					elem = elem.offsetParent;
-				}
-				return { top: top, left: left };
-			}
-		},
-		expandEvent: function (e) {
-			var from = e.changedTouches ? e.changedTouches[0] : e;
-			if (!('page' in e) || !('offset' in e)) {
-				e.page = from.page || {
-					x: 'pageX' in from ? from.pageX : from.clientX + document.scrollLeft,
-					y: 'pageY' in from ? from.pageY : from.clientY + document.scrollTop
-				};
-				if ('offsetX' in from) {
-					e.offset = new Point(from.offsetX, from.offsetY);
-				} else {
-					var offset = LibCanvas.Mouse.createOffset(from.target);
-					e.offset = new Point({
-						x: e.page.x - offset.left,
-						y: e.page.y - offset.top
-					});
-					e.offsetX = e.offset.x;
-					e.offsetY = e.offset.y;
-				}
-			}
-			return e;
-		},
-		getOffset : function (e) {
-			if (!e.offset) LibCanvas.Mouse.expandEvent(e);
-			return e.offset;
-		}
-	},
-	proto: {
-		initialize : function (elem) {
-			this.inCanvas = false;
-			this.point = new Point(null, null);
-			/** @private */
-			this.prev  = new Point(null, null);
-			/** @private */
-			this.diff  = new Point(null, null);
-
-			this.elem = elem;
-
-			this.events  = new Events(this);
-			this.handler = new MouseEvents(this);
-
-			this.setEvents();
-		},
-		isOver: function (elem) {
-			var translate = elem.mouseTranslate;
-			if (translate) this.point.move( translate, true );
-			var result = this.inCanvas && elem.hasPoint( this.point );
-			if (translate) this.point.move( translate );
-			return result;
-		},
-		button: function (key) {
-			return this.constructor.buttons[key || 'left'];
-		},
-		setCoords : function (point, inCanvas) {
-			this.prev.set( this.point );
-			this.diff = this.prev.diff( point );
-			this.inCanvas = inCanvas;
-			this.point.move( this.diff );
-			this.debugUpdate();
-			return this;
-		},
-		getOffset : function (e) {
-			return LibCanvas.Mouse.getOffset(e);
-		},
-		setEvents : function () {
-
-			// e.previousOffset = prev.clone();
-			// e.deltaOffset    = prev.diff( this.point );
-			var mouse = this,
-			waitEvent = function (event, isOffice) {
-				if (event.match(/^mouse/)) {
-					var shortE = event.substr(5);
-				}
-
-				return function (e) {
-					prepare( e );
-					var wait     = mouse.isEventAdded(event),
-						waitShort = ( shortE && mouse.isEventAdded(shortE) );
-					if (isOffice || wait || waitShort) mouse.getOffset(e);
-
-					if (isOffice ) mouse.handler.event(event, e);
-					if (wait     ) mouse.events.fire(event, [e]);
-					if (waitShort) mouse.events.fire(shortE, [e]);
-					if (isOffice ) e.preventDefault();
-
-					return !isOffice;
-				};
+				return e;
 			},
-			waitWheel = waitEvent('wheel', false),
-			prepare = function (e) {
-				e.previousOffset = mouse.prev;
-				e.deltaOffset = mouse.diff;
-			},
-			wheel = function (e) {
-				e.delta =
-					// IE, Opera, Chrome
-					e.wheelDelta ? e.wheelDelta > 0 ? 1 : -1 :
-					// Fx
-					e.detail     ? e.detail     < 0 ? 1 : -1 : null;
-				e.up   = e.delta > 0;
-				e.down = e.delta < 0;
-				waitWheel(e);
-				mouse.handler.event('wheel', e);
-			},
-			down = waitEvent('mousedown', true),
-			up   = waitEvent('mouseup'  , true),
-			over = waitEvent('mouseover', true),
-			move = function ( e ) {
-				var offset = mouse.getOffset(e);
-				mouse.setCoords(offset, true);
-				prepare( e );
-				mouse.handler.event('mousemove', e);
-				mouse.events.fire('move', [e]);
-				mouse.isOut = false;
-				e.preventDefault();
-				return false;
-			},
-			out = function (e) {
-				var offset = mouse.getOffset(e);
-				mouse.setCoords(offset, false);
-				prepare( e );
-				mouse.handler.event('mouseout', e);
-				mouse.events.fire('mouseout', [e]);
-				mouse.events.fire('out', [e]);
-				mouse.isOut = true;
-				e.preventDefault();
-				return false;
-			};
+			getOffset : function (e, element) {
+				var elementOffset = atom.dom(element || eventSource(e).target).offset();
 
-			atom.dom(mouse.elem).bind({
-				click      : waitEvent('click', false),
-				dblclick   : waitEvent('dblclick', false),
-				contextmenu: waitEvent('contextmenu', false),
-				mouseover  : over,
-				mousedown  : down,
-				mouseup    : up,
-				mousemove  : move,
-				mouseout   : out,
-				selectstart: false,
-				DOMMouseScroll: wheel,
-				mousewheel: wheel
-			});
-			return this;
-		},
-		subscribe : function (elem) {
-			this.handler.subscribe(elem);
-			return this;
-		},
-		unsubscribe : function (elem) {
-			this.handler.unsubscribe(elem);
-			return this;
-		},
-		debugTrace: null,
-		debugUpdate: function () {
-			if (this.debugTrace) {
-				this.debugTrace.trace( 'Mouse' +
-					(this.inCanvas ? ': ' + this.point.x.round() + ',' + this.point.y.round() : ' is out of canvas')
+				this.expandEvent(e);
+
+				return new Point(
+					e.pageX - elementOffset.x,
+					e.pageY - elementOffset.y
 				);
 			}
 		},
-		debug : function (on) {
-			if (on && !this.debugTrace) {
-				this.debugTrace = new Trace();
-			} else if (on === false) {
-				this.debugTrace = null;
-			}
-			this.debugUpdate();
-			return this;
-		},
-		dump: function () {
-			var p = this.point;
-			return '[Mouse(' + p.x + '*' + p.y + ')]';
-		},
-		toString: Function.lambda('[object LibCanvas.Mouse]')
-	}
-});
 
-LibCanvas.Mouse.listen();
+		prototype: {
+			/** @private */
+			elem: null,
+
+			/** @property {boolean} */
+			inside: false,
+			/** @property {Point} */
+			point: null,
+			/** @property {Point} */
+			previous: null,
+			/** @property {Point} */
+			delta: null,
+			/** @property {Events} */
+			events: null,
+
+			/** @private */
+			mapping: {
+				click      : 'click',
+				dblclick   : 'dblclick',
+				contextmenu: 'contextmenu',
+
+				mouseover  : 'over',
+				mousedown  : 'down',
+				mouseup    : 'up',
+				mousemove  : 'move',
+				mouseout   : 'out',
+
+				DOMMouseScroll: 'wheel',
+				mousewheel    : 'wheel'
+			},
+
+			initialize : function (elem, offsetElem) {
+				this.bindMethods( 'onEvent' );
+
+				this.elem       = atom.dom(elem);
+				this.offsetElem = atom.dom(offsetElem) || this.elem;
+
+				this.point    = new Point(0, 0);
+				this.previous = new Point(0, 0);
+				this.delta    = new Point(0, 0);
+				this.events   = new Events(this);
+
+				this.listen(this.onEvent);
+			},
+			/** @private */
+			onEvent: function (e) {
+				var
+					event = this.mapping[e.type],
+					fn    = this.eventActions[event];
+
+				if (fn) fn.call( this, e );
+
+				this.events.fire(this.mapping[event], [e, this]);
+			},
+			/** @private */
+			getOffset: function (e) {
+				return this.constructor.getOffset(e, this.offsetElem);
+			},
+			/** @private */
+			set: function (e, inside) {
+				var point = this.getOffset(e);
+
+				this.prev .set( this.point );
+				this.delta.set( this.prev.diff( point ) );
+				this.point.set( point );
+				this.inside = inside;
+			},
+			/** @private */
+			eventActions: {
+				wheel: function (e) {
+					e.delta =
+						// IE, Opera, Chrome
+						e.wheelDelta ? e.wheelDelta > 0 ? 1 : -1 :
+						// Fx
+						e.detail     ? e.detail     < 0 ? 1 : -1 : null;
+				},
+
+				move: function (e) {
+					this.set(e, true);
+				},
+
+				up: function (e) {
+					this.set(e, false);
+				}
+			},
+			/** @private */
+			listen : function (callback) {
+				this.elem.bind({
+					click      : callback,
+					dblclick   : callback,
+					contextmenu: callback,
+
+					mouseover  : callback,
+					mousedown  : callback,
+					mouseup    : callback,
+					mousemove  : callback,
+					mouseout   : callback,
+
+					DOMMouseScroll: callback,
+					mousewheel    : callback
+				});
+			}
+		}
+	});
+};
