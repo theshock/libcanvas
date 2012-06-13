264676c0bbf83f33f7e4c48d3992afda54dcccca
TheShock <shocksilien@gmail.com>
2012-06-13 20:48:31 +0300

    Recompile

--- libcanvas-full-compiled.js
+++ libcanvas-full-compiled.js
@@ -1198,6 +1198,70 @@ declare( 'LibCanvas.App.MouseHandler', {
 /*
 ---
 
+name: "Core.Canvas"
+
+description: "Provides some Canvas extensions"
+
+license:
+	- "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
+	- "[MIT License](http://opensource.org/licenses/mit-license.php)"
+
+authors:
+	- "Shock <shocksilien@gmail.com>"
+
+requires:
+	- LibCanvas
+
+provides: Core.Canvas
+
+...
+*/
+
+atom.core.append(HTMLCanvasElement,
+/** @lends HTMLCanvasElement */
+{
+	/** @private */
+	_newContexts: {},
+	/** @returns {HTMLCanvasElement} */
+	addContext: function (name, ctx) {
+		this._newContexts[name] = ctx;
+		return this;
+	},
+	/** @returns {Context2D} */
+	getContext: function (name) {
+		return this._newContexts[name] || null;
+	}
+});
+
+atom.core.append(HTMLCanvasElement.prototype,
+/** @lends HTMLCanvasElement.prototype */
+{
+	getOriginalContext: HTMLCanvasElement.prototype.getContext,
+	/** @returns {Context2D} */
+	getContext: function (type) {
+		if (!this.contextsList) {
+			this.contextsList = {};
+		}
+
+		if (!this.contextsList[type]) {
+			var ctx = HTMLCanvasElement.getContext(type);
+			if (ctx) {
+				ctx = new ctx(this);
+			} else try {
+				ctx = this.getOriginalContext.apply(this, arguments);
+			} catch (e) {
+				throw (!e.toString().match(/NS_ERROR_ILLEGAL_VALUE/)) ? e :
+					new TypeError('Wrong Context Type: «' + type + '»');
+			}
+			this.contextsList[type] = ctx;
+		}
+		return this.contextsList[type];
+	}
+});
+
+/*
+---
+
 name: "Geometry"
 
 description: "Base for such things as Point and Shape"
@@ -1921,70 +1985,6 @@ var Circle = LibCanvas.declare( 'LibCanvas.Shapes.Circle', 'Circle', Shape, {
 /*
 ---
 
-name: "Utils.Canvas"
-
-description: "Provides some Canvas extensions"
-
-license:
-	- "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
-	- "[MIT License](http://opensource.org/licenses/mit-license.php)"
-
-authors:
-	- "Shock <shocksilien@gmail.com>"
-
-requires:
-	- LibCanvas
-
-provides: Utils.Canvas
-
-...
-*/
-
-atom.core.append(HTMLCanvasElement,
-/** @lends HTMLCanvasElement */
-{
-	/** @private */
-	_newContexts: {},
-	/** @returns {HTMLCanvasElement} */
-	addContext: function (name, ctx) {
-		this._newContexts[name] = ctx;
-		return this;
-	},
-	/** @returns {Context2D} */
-	getContext: function (name) {
-		return this._newContexts[name] || null;
-	}
-});
-
-atom.core.append(HTMLCanvasElement.prototype,
-/** @lends HTMLCanvasElement.prototype */
-{
-	getOriginalContext: HTMLCanvasElement.prototype.getContext,
-	/** @returns {Context2D} */
-	getContext: function (type) {
-		if (!this.contextsList) {
-			this.contextsList = {};
-		}
-
-		if (!this.contextsList[type]) {
-			var ctx = HTMLCanvasElement.getContext(type);
-			if (ctx) {
-				ctx = new ctx(this);
-			} else try {
-				ctx = this.getOriginalContext.apply(this, arguments);
-			} catch (e) {
-				throw (!e.toString().match(/NS_ERROR_ILLEGAL_VALUE/)) ? e :
-					new TypeError('Wrong Context Type: «' + type + '»');
-			}
-			this.contextsList[type] = ctx;
-		}
-		return this.contextsList[type];
-	}
-});
-
-/*
----
-
 name: "Context2D"
 
 description: "LibCanvas.Context2D adds new canvas context '2d-libcanvas'"
@@ -2002,7 +2002,7 @@ requires:
 	- Size
 	- Shapes.Rectangle
 	- Shapes.Circle
-	- Utils.Canvas
+	- Core.Canvas
 
 provides: Context2D
 
@@ -2151,7 +2151,9 @@ var Context2D = LibCanvas.declare( 'LibCanvas.Context2D', 'Context2D',
 			this.canvas = this.ctx2d.canvas;
 		} else {
 			this.canvas = canvas;
-			this.ctx2d  = canvas.getOriginalContext('2d');
+			this.ctx2d  = atom.core.isFunction(canvas.getOriginalContext) ?
+				canvas.getOriginalContext('2d') :
+				canvas.getContext('2d');
 		}
 	},
 	get width () { return this.canvas.width; },
@@ -3570,6 +3572,7 @@ LibCanvas.declare( 'LibCanvas.Engines.IsometricProjection', 'IsometricProjection
 
  requires:
  - LibCanvas
+ - Plugins.Image
 
  ...
  */
@@ -4059,6 +4062,151 @@ Context2D.prototype.drawCurve = function (obj) {
 };
 
 /*
+---
+
+name: "Plugins.Image"
+
+description: "Provides some Image extensions"
+
+license:
+	- "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
+	- "[MIT License](http://opensource.org/licenses/mit-license.php)"
+
+authors:
+	- "Shock <shocksilien@gmail.com>"
+
+requires:
+	- LibCanvas
+	- Shapes.Rectangle
+
+provides: Plugins.Image
+
+...
+*/
+
+var UtilsImage = atom.declare( 'LibCanvas.Utils.Image', {
+	canvasCache: null,
+
+	initialize: function (image) {
+		this.image = image;
+		this.cache = {};
+	},
+
+	/**
+	 * @param {Rectangle} rect
+	 * #todo: use createPattern
+	 */
+	createSprite: function (rect) {
+		var image, buf, xShift, yShift, x, y, xMax, yMax, crop, size, current, from, to;
+
+		if (rect.width <= 0 || rect.height <= 0) {
+			throw new TypeError('Wrong rectangle size');
+		}
+
+		image = this.image;
+		buf = LibCanvas.buffer(rect.width, rect.height, true);
+
+		// если координаты выходят за левый/верхний край картинки
+		if (rect.from.x < 0) xShift = Math.ceil(Math.abs(rect.from.x) / rect.width );
+		if (rect.from.y < 0) yShift = Math.ceil(Math.abs(rect.from.y) / rect.height);
+		if (xShift || yShift) {
+			rect = rect.clone().move(new Point(
+				xShift * image.width,
+				yShift * image.height
+			));
+		}
+
+		// для того, чтобы была возможность указывать ректангл, выходящий
+		// за пределы картинки. текущая картинка повторяется как паттерн
+		xMax = Math.ceil(rect.to.x / image.width );
+		yMax = Math.ceil(rect.to.y / image.height);
+		for (y = yMax; y-- > 0;) for (x = xMax; x-- > 0;) {
+			current = new Point(x * image.width, y * image.height);
+			from = current.clone();
+			to   = from.clone().move([image.width, image.height]);
+
+			if (from.x < rect.from.x) from.x = rect.from.x;
+			if (from.y < rect.from.y) from.y = rect.from.y;
+			if (  to.x > rect. to .x)   to.x = rect. to .x;
+			if (  to.y > rect. to .y)   to.y = rect. to .y;
+
+			crop = new Rectangle(from, to);
+			size = crop.size;
+			crop.from.x %= image.width;
+			crop.from.y %= image.height;
+			crop.size    = size;
+
+			if (x) current.x -= rect.from.x;
+			if (y) current.y -= rect.from.y;
+
+			if (size.width && size.height) buf.ctx.drawImage({
+				image : image,
+				crop  : crop,
+				draw  : new Rectangle( current, size )
+			});
+		}
+
+		return buf;
+	},
+
+	toCanvas: function () {
+		var cache = this.canvasCache;
+
+		if (!cache) {
+			cache = this.canvasCache = LibCanvas.buffer(this, true);
+			cache.ctx.drawImage(this);
+		}
+		return cache;
+	},
+
+	isLoaded: function () {
+		return this.constructor.isLoaded( this.image );
+	},
+
+	sprite: function () {
+		if (!this.isLoaded()) throw new Error('Not loaded in Image.sprite, logged');
+
+		if (arguments.length) {
+			var
+				rect  = Rectangle(arguments),
+				index = rect.dump(),
+				cache = this.cache[index];
+
+			if (!cache) {
+				cache = this.cache[index] = this.createSprite(rect);
+			}
+			return cache;
+		} else {
+			return this.toCanvas();
+		}
+	}
+});
+
+UtilsImage.own({
+	isLoaded : function (image) {
+		return image.complete && ( (image.naturalWidth == null) || !!image.naturalWidth );
+	},
+
+	sprite: function (image, rectangle) {
+		return this.mix(image).sprite(rectangle);
+	},
+
+	toCanvas: function (image) {
+		return this.mix(image).toCanvas();
+	},
+
+	mix: function (image) {
+		var key = 'libcanvas.image';
+
+		if (!image[key]) {
+			image[key] = new this(image);
+		}
+
+		return image[key];
+	}
+});
+
+/*
  ---
 
  name: "ImageBuilder"
@@ -4300,6 +4448,55 @@ atom.declare( 'LibCanvas.Plugins.ImageBuilder.Vertical', ImageBuilder, {
 
 		this.images = images;
 	}
+});
+
+/*
+---
+
+name: "Plugins.ImagePrototype"
+
+description: "Provides some Image extensions"
+
+license:
+	- "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
+	- "[MIT License](http://opensource.org/licenses/mit-license.php)"
+
+authors:
+	- "Shock <shocksilien@gmail.com>"
+
+requires:
+	- LibCanvas
+	- Utils.Image
+
+provides: Plugins.ImagePrototype
+
+...
+*/
+
+// <image> tag
+atom.core.append(HTMLImageElement.prototype, {
+	createSprite: function (rect) {
+		return UtilsImage.mix(this).createSprite(rect);
+	},
+	toCanvas: function () {
+		return UtilsImage.mix(this).toCanvas();
+	},
+	sprite : function () {
+		var utils = UtilsImage.mix(this);
+
+		return utils.sprite.apply( utils, arguments );
+	},
+	isLoaded : function () {
+		return UtilsImage.isLoaded(this);
+	}
+});
+
+// mixin from image
+atom.core.append(HTMLCanvasElement.prototype, {
+	createSprite : HTMLImageElement.prototype.createSprite,
+	sprite   : HTMLImageElement.prototype.sprite,
+	isLoaded : atom.fn.lambda(true),
+	toCanvas : atom.fn.lambda()
 });
 
 /*
@@ -5951,199 +6148,6 @@ var RoundedRectangle = LibCanvas.declare(
 			var p = function (p) { return '[' + p.x + ', ' + p.y + ']'; };
 			return '[shape RoundedRectangle(from'+p(this.from)+', to'+p(this.to)+', radius='+this.radius+')]';
 		}
-});
-
-/*
----
-
-name: "Utils.Image"
-
-description: "Provides some Image extensions"
-
-license:
-	- "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
-	- "[MIT License](http://opensource.org/licenses/mit-license.php)"
-
-authors:
-	- "Shock <shocksilien@gmail.com>"
-
-requires:
-	- LibCanvas
-	- Shapes.Rectangle
-
-provides: Utils.Image
-
-...
-*/
-
-var UtilsImage = atom.declare( 'LibCanvas.Utils.Image', {
-	canvasCache: null,
-
-	initialize: function (image) {
-		this.image = image;
-		this.cache = {};
-	},
-
-	/**
-	 * @param {Rectangle} rect
-	 * #todo: use createPattern
-	 */
-	createSprite: function (rect) {
-		var image, buf, xShift, yShift, x, y, xMax, yMax, crop, size, current, from, to;
-
-		if (rect.width <= 0 || rect.height <= 0) {
-			throw new TypeError('Wrong rectangle size');
-		}
-
-		image = this.image;
-		buf = LibCanvas.buffer(rect.width, rect.height, true);
-
-		// если координаты выходят за левый/верхний край картинки
-		if (rect.from.x < 0) xShift = Math.ceil(Math.abs(rect.from.x) / rect.width );
-		if (rect.from.y < 0) yShift = Math.ceil(Math.abs(rect.from.y) / rect.height);
-		if (xShift || yShift) {
-			rect = rect.clone().move(new Point(
-				xShift * image.width,
-				yShift * image.height
-			));
-		}
-
-		// для того, чтобы была возможность указывать ректангл, выходящий
-		// за пределы картинки. текущая картинка повторяется как паттерн
-		xMax = Math.ceil(rect.to.x / image.width );
-		yMax = Math.ceil(rect.to.y / image.height);
-		for (y = yMax; y-- > 0;) for (x = xMax; x-- > 0;) {
-			current = new Point(x * image.width, y * image.height);
-			from = current.clone();
-			to   = from.clone().move([image.width, image.height]);
-
-			if (from.x < rect.from.x) from.x = rect.from.x;
-			if (from.y < rect.from.y) from.y = rect.from.y;
-			if (  to.x > rect. to .x)   to.x = rect. to .x;
-			if (  to.y > rect. to .y)   to.y = rect. to .y;
-
-			crop = new Rectangle(from, to);
-			size = crop.size;
-			crop.from.x %= image.width;
-			crop.from.y %= image.height;
-			crop.size    = size;
-
-			if (x) current.x -= rect.from.x;
-			if (y) current.y -= rect.from.y;
-
-			if (size.width && size.height) buf.ctx.drawImage({
-				image : image,
-				crop  : crop,
-				draw  : new Rectangle( current, size )
-			});
-		}
-
-		return buf;
-	},
-
-	toCanvas: function () {
-		var cache = this.canvasCache;
-
-		if (!cache) {
-			cache = this.canvasCache = LibCanvas.buffer(this, true);
-			cache.ctx.drawImage(this);
-		}
-		return cache;
-	},
-
-	isLoaded: function () {
-		return this.constructor.isLoaded( this.image );
-	},
-
-	sprite: function () {
-		if (!this.isLoaded()) throw new Error('Not loaded in Image.sprite, logged');
-
-		if (arguments.length) {
-			var
-				rect  = Rectangle(arguments),
-				index = rect.dump(),
-				cache = this.cache[index];
-
-			if (!cache) {
-				cache = this.cache[index] = this.createSprite(rect);
-			}
-			return cache;
-		} else {
-			return this.toCanvas();
-		}
-	}
-});
-UtilsImage.own({
-	isLoaded : function (image) {
-		return image.complete && ( (image.naturalWidth == null) || !!image.naturalWidth );
-	},
-
-	sprite: function (image, rectangle) {
-		return this.mix(image).sprite(rectangle);
-	},
-
-	toCanvas: function (image) {
-		return this.mix(image).toCanvas();
-	},
-
-	mix: function (image) {
-		var key = 'libcanvas.image';
-
-		if (!image[key]) {
-			image[key] = new this(image);
-		}
-
-		return image[key];
-	}
-});
-
-/*
----
-
-name: "Utils.ImagePrototype"
-
-description: "Provides some Image extensions"
-
-license:
-	- "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
-	- "[MIT License](http://opensource.org/licenses/mit-license.php)"
-
-authors:
-	- "Shock <shocksilien@gmail.com>"
-
-requires:
-	- LibCanvas
-	- Utils.Image
-
-provides: Utils.ImagePrototype
-
-...
-*/
-
-// <image> tag
-atom.core.append(HTMLImageElement.prototype, {
-	createSprite: function (rect) {
-		return UtilsImage.mix(this).createSprite(rect);
-	},
-	toCanvas: function () {
-		return UtilsImage.mix(this).toCanvas();
-	},
-	sprite : function () {
-		var utils = UtilsImage.mix(this);
-
-		return utils.sprite.apply( utils, arguments );
-	},
-	isLoaded : function () {
-		return UtilsImage.isLoaded(this);
-	}
-});
-
-// mixin from image
-atom.core.append(HTMLCanvasElement.prototype, {
-	createSprite : HTMLImageElement.prototype.createSprite,
-	sprite   : HTMLImageElement.prototype.sprite,
-	isLoaded : atom.fn.lambda(true),
-	toCanvas : atom.fn.lambda()
 });
 
 /*
