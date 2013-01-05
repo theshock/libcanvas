8725e698b9c2c3f9dfba5f4c78774404b0d4d44e
TheShock <shocksilien@gmail.com>
2013-01-05 05:03:19 +0200

    Plugins.Curve in subdirecory

---
+++ Source/Plugins/Curve/Quadratic.js
@@ -0,0 +1,39 @@
+/*
+ ---
+
+ name: "Plugins.Curve"
+
+ description: "Provides math base for bezier curves"
+
+ license:
+ - "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
+ - "[MIT License](http://opensource.org/licenses/mit-license.php)"
+
+ authors:
+ - Pavel Ponomarenko aka Shock <shocksilien@gmail.com>
+
+ provides: Plugins.Curve.Quadratic
+
+ requires:
+ - Plugins.Curve
+
+ ...
+ */
+
+/** @name LibCanvas.Plugins.Curve.Quadratic */
+atom.declare( 'LibCanvas.Plugins.Curve.Quadratic', LibCanvas.Plugins.Curve, {
+
+	getPoint: function (t) {
+		var
+			from = this.from,
+			to   = this.to,
+			point= this.cp[0],
+			i    = 1 - t;
+
+		return new Point(
+			i*i*from.x + 2*t*i*point.x + t*t*to.x,
+			i*i*from.y + 2*t*i*point.y + t*t*to.y
+		);
+	}
+
+});
---
+++ Source/Plugins/Curve/Qubic.js
@@ -0,0 +1,39 @@
+/*
+ ---
+
+ name: "Plugins.Curve.Qubic"
+
+ description: "Provides math base for bezier curves"
+
+ license:
+ - "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
+ - "[MIT License](http://opensource.org/licenses/mit-license.php)"
+
+ authors:
+ - Pavel Ponomarenko aka Shock <shocksilien@gmail.com>
+
+ provides: Plugins.Curve.Qubic
+
+ requires:
+ - Plugins.Curve
+
+ ...
+ */
+
+/** @name LibCanvas.Plugins.Curve.Qubic */
+atom.declare( 'LibCanvas.Plugins.Curve.Qubic', LibCanvas.Plugins.Curve, {
+
+	getPoint: function (t) {
+		var
+			from = this.from,
+			to   = this.to,
+			cp   = this.cp,
+			i    = 1 - t;
+
+		return new Point(
+			i*i*i*from.x + 3*t*i*i*cp[0].x + 3*t*t*i*cp[1].x + t*t*t*to.x,
+			i*i*i*from.y + 3*t*i*i*cp[0].y + 3*t*t*i*cp[1].y + t*t*t*to.y
+		);
+	}
+
+});
